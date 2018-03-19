721d78cf269e516a8134788f7077652d1df4a313
Eric <eric-noren@hotmail.com>
2018-03-19 12:14:07 +0100

    Added static access to agents
    CHANGE COPYING TO SERIALIZATION INSTEAD

--- BTEvolve/Assets/StaticGlobals.cs
+++ BTEvolve/Assets/StaticGlobals.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StaticGlobals : MonoBehaviour {
 
     public MeshRenderer plane;
+    public GameObject agent0, agent1;
 
     public Vector3 GetPlaneMax()
     {
@@ -17,5 +18,13 @@ public class StaticGlobals : MonoBehaviour {
     public MeshRenderer GetPlane()
     {
         return plane;
+    }
+    public ShooterAgent GetAgent0()
+    {
+        return agent0.GetComponent<ShooterAgent>();
+    }
+    public ShooterAgent GetAgent1()
+    {
+        return agent1.GetComponent<ShooterAgent>();
     }
 }
--- BTEvolve/Assets/StaticMethods.cs
+++ BTEvolve/Assets/StaticMethods.cs
@@ -4,7 +4,34 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
-public static class StaticMethods {
+// Static singleton staticmethods
+public class StaticMethods {
+
+    private static StaticMethods instance;
+    private GameObject agent0, agent1;
+
+    private StaticMethods()
+    {
+        agent0 = GameObject.Find("agent0");
+        agent1 = GameObject.Find("agent1");
+    }
+    
+    public static StaticMethods GetInstance()
+    {
+        if (instance == null)
+            instance = new StaticMethods();
+
+        return instance;
+    }
+
+    public ShooterAgent GetAgent0 ()
+    {
+        return agent0.GetComponent<ShooterAgent>();
+    }
+    public ShooterAgent GetAgent1 ()
+    {
+        return agent1.GetComponent<ShooterAgent>();
+    }
 
     // Create a deep copy of a given object.
     public static T DeepCopy<T>(T other)
